b83df477a7f852ae29f118ce7ed21e555c41b05e
Your Name <you@example.com>
2021-06-15 14:44:40 +0300

    manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,19 @@
+package manager;
+
+public class PostManager {
+private Post[] posts;
+
+
+    public  Post[] search(int ownerId, string domain, string query, boolean ownersOnly, int count, int offset) {
+
+
+        //TODO; ADD LOGIS
+        return null;
+
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+
+}
