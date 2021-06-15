5824f8a20b2c568251f0546541b99fc1969a621a
Your Name <you@example.com>
2021-06-15 14:00:59 +0300

    1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,24 @@
+package domain;
+
+
+
+public class Post {
+    /*private Advertising advertising;
+    private Donut donut;
+    private Favourite favourite;
+    private Geo geo;
+    private Like like;
+    private Place place;
+    private Placeholder placeholder;
+    private PostAction postAction;*/
+    private PostInfo postInfo;
+    /*private PostponePost postponePost;*/
+    private PostTime postTime;
+   /* private Privacy privacy;*/
+    private Repost repost;
+    /*private ShowPostName showPostName;
+    private ViewComment viewComment;
+    private ViewReply viewReply;
+    private Views views;
+*/
+}
---
+++ src/main/java/domain/PostTime.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class PostTime {
+    private int date;
+    // + get/set на все поля
+}
+
+
