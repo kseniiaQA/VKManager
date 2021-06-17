c410094d3dcd1fb380a74e7fb3efbe724c262635
Your Name <you@example.com>
2021-06-17 14:36:54 +0300

    class Post updated

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -3,22 +3,22 @@ package domain;
 
 
 public class Post {
-    /*private Advertising advertising;
+    private Advertising advertising;
     private Donut donut;
     private Favourite favourite;
     private Geo geo;
     private Like like;
     private Place place;
     private Placeholder placeholder;
-    private PostAction postAction;*/
+    private PostAction postAction;
     private PostInfo postInfo;
-    /*private PostponePost postponePost;*/
+   private PostponePost postponePost;
     private PostTime postTime;
-   /* private Privacy privacy;*/
+   private Privacy privacy;
     private Repost repost;
-    /*private ShowPostName showPostName;
+   private ShowPostName showPostName;
     private ViewComment viewComment;
     private ViewReply viewReply;
     private Views views;
-*/
+
 }
